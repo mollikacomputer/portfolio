825f92f7d939403b3a305677d08cb9adf93a14f3
mollikacomputer <mollikacomputer3@gmail.com>
2022-06-10 22:52:17 +0600

    target blank added

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 const Project = ({projectInfo}) => {
   return (
     <div className="card bg-base-100">
       <figure>
+        <a href={projectInfo.liveLink} target="_blank" >
         <img
           src={projectInfo.pic}
           alt="Album"
+          style={{height: "250px"}}
         />
+        </a>
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title"> {projectInfo.name} </h2>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,18 +6,21 @@ const Projects = () => {
         {
           id: 0,
           pic: `https://i.ibb.co/MsZKyrf/1car-parts.jpg`,
+          liveLink: `https://cheerful-puppy-d7cc0f.netlify.app`,
           name: "Countries",
           description:'description'
         },
         {
           id: 1,
           pic: `https://i.ibb.co/crnZxBx/2grocery-Dealer.jpg`,
+          liveLink: `https://heroic-moxie-7c88c8.netlify.app`,
           name: "Complete projects",
           description:'description'
         },
         {
           id: 2,
           pic: `https://i.ibb.co/4S9Jzg4/3private-tutor.jpg`,
+          liveLink: `https://fascinating-licorice-2885a9.netlify.app`,
           name: "happy clients",
           description:'description'
         },
